5c2be8020e36f2636c42a161ca9274981706f4dc
ZhiCheng Liu <zhicheng.liu@cn.abb.com>
2019-10-21 22:50:50 +0800

    添加部署相关信息

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+// pm2的配置文件
+module.exports = {
+  // app是pm2执行命令的时候, 执行哪些应用, 是一个数组, 有多少个应用实例就有多少个对象
+  app: [
+    {
+      name: 'next-project',
+      script: './server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}
